15106dce9b3678c8d7b67378bfcab6280a32af27
Shahroon Ali <shahroon.ali@gmail.com>
2013-11-16 16:26:28 +0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Optimus::Application.config.secret_token = '672f485ed94dbdec8ea76a8e76d0f53c6a4b9b8ade6835ba7276d4ace8ba47f9e2eb6b1ad7e44ef4b6a1dac552911767a28818150312f8e227c6d61de3c275c3'
